41388b54e86675101255db17b201c8ea229e571e
toticavalcanti <toticavalcanti@hotmail.com>
2018-06-28 20:04:07 -0300

    set service_raffle

--- spec/services/raffle_service_spec.rb
+++ spec/services/raffle_service_spec.rb
@@ -22,18 +22,18 @@ describe RaffleService do
       end
 
       it "all members are in results as a member" do
-        result_members = @results.map {|member, friend| member}
+        result_members = @results.map {|r| r.first}
         expect(result_members.sort).to eq(@campaign.members.sort)
       end
 
       it "all member are in results as a friend" do
-        result_friends = @results.map {|member, friend| friend}
+        result_friends = @results.map {|r| r.last}
         expect(result_friends.sort).to eq(@campaign.members.sort)
       end
 
       it "a member don't get yourself" do
-        @results.each do |member, friend|
-          expect(member).not_to eq(friend)
+        @results.each do |r|
+          expect(r.first).not_to eq(r.last)
         end
       end
 
